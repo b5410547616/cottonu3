448b037d7280f2c3f339a529c66f5bee185de972
Nuttapon <nuttapon.phannurat@isobar.com>
2017-06-12 17:16:49 +0700

    Initialize

---
+++ server.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var path = require('path');
+
+var socketio = require('socket.io');
+var express = require('express');
+ 
+var router = express();
+var server = http.createServer(router);
+var io = socketio.listen(server);
+
+router.use(express.static(path.resolve(__dirname, 'client')));
+
+const helmet = require('helmet');
+const app = express();
+app.use(helmet());
+
+var items = [];
+
+io.on('connection', function (socket) {
+  socket.emit('news', { status: 'connected' });
+  socket.emit('update', items);
+
+  socket.on('receiveData', function (data) {
+  	console.log("receiveData", data);
+
+    for (var i = data.length - 1 ; i >= 0 ; i--) {
+      items.unshift(data[i]);
+    }
+
+    socket.broadcast.emit('clientData', items);
+  });
+
+  socket.on('resetItem', function (data) {
+    console.log("receiveData", data);
+    socket.broadcast.emit('resetClient', data);
+  });
+
+});
+
+
+server.listen(8888, function(){
+  console.log('listening on *:8888');
+});
